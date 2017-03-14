55269388e9edf473df43b0b9deceb69e2e058b17
Matt Langford <matthew.langford95@gmail.com>
2017-03-13 20:26:31 -0400

    TOOLS: Fix mil tools from great merge

--- scripts/two_line_bash.sh
+++ scripts/two_line_bash.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+#
+#
+# This was casuing issues in tmux and always showed an "catkin_mil_ws", I'm sure that was intended
+# but it's kind of annoying
+#
+#
+
 # Two line bash is a script that adds separators between commands to make
 # their output more readable. It also includes the current time and the
 # selected branch of the git repository the user is in. While it is enabled
@@ -8,46 +15,79 @@
 # command: touch ~/.disable_tlb
 
 
-function parse_catkin_workspace {
-	PS_WORKSPACE=""
-	local WORKSPACE_DIR="`echo $ROS_PACKAGE_PATH | cut -d ':' -f1 | sed 's@/src@@'`"
-	if [ -f $WORKSPACE_DIR/.catkin_workspace ]; then
-		PS_WORKSPACE="(catkin `echo $WORKSPACE_DIR | rev | cut -d "/" -f1 | rev`) "
-	fi
-}
+# function parse_catkin_workspace {
+# 	PS_WORKSPACE=""
+# 	local WORKSPACE_DIR="`echo $ROS_PACKAGE_PATH | cut -d ':' -f1 | sed 's@/src@@'`"
+# 	if [ -f $WORKSPACE_DIR/.catkin_workspace ]; then
+# 		PS_WORKSPACE="(catkin `echo $WORKSPACE_DIR | rev | cut -d "/" -f1 | rev`) "
+# 	fi
+# }
+# 
+# function parse_git_branch {
+# 	PS_BRANCH=""
+# 	if [ -d .svn ]; then
+# 		PS_BRANCH="(svn r$(svn info|awk '/Revision/{print $2}')) "
+# 		return
+# 	elif [ -f _FOSSIL_ -o -f .fslckout ]; then
+# 		PS_BRANCH="(fossil $(fossil status|awk '/tags/{print $2}')) "
+# 		return
+# 	fi
+# 	ref=$(git symbolic-ref HEAD 2> /dev/null) || return
+# 	PS_BRANCH="(git ${ref#refs/heads/}) "
+# }
+# 
+# function parse_tlb_info {
+# 	parse_catkin_workspace
+# 	parse_git_branch
+# }
+# 
+# 
+# if [ ! -f ~/.disable_tlb ]; then
+# 	RESET="\[\033[0m\]"
+# 	RED="\[\033[0;31m\]"
+# 	GREEN="\[\033[01;32m\]"
+# 	BLUE="\[\033[01;34m\]"
+# 	YELLOW="\[\033[0;33m\]"
+# 
+# 	PROMPT_COMMAND=parse_tlb_info
+# 	PS_LINE=`printf -- "- %.0s" {1..200}`
+# 	PS_FILL=${PS_LINE:0:$COLUMNS}
+# 	PS_INFO="$GREEN\u@\h$RESET:$BLUE\w "
+# 	PS_CATKIN="$YELLOW\$PS_WORKSPACE"
+# 	PS_GIT="$YELLOW\$PS_BRANCH"
+# 	PS_TIME="\[\033[\$((COLUMNS-10))G\] $RED[\t]"
+# 	export PS1="\${PS_FILL}\[\033[0G\]${PS_INFO}${PS_GIT}${PS_TIME}\n${RESET}\$ "
+# fi
 
+# ===========================================================
+# A two-line colored Bash prompt (PS1) with Git branch and a line decoration
+# which adjusts automatically to the width of the terminal.
+# Recognizes and shows Git, SVN and Fossil branch/revision.
+# Screenshot: http://img194.imageshack.us/img194/2154/twolineprompt.png
+# Michal Kottman, 2012
+
+RESET="\[\033[0m\]"
+RED="\[\033[0;31m\]"
+GREEN="\[\033[01;32m\]"
+BLUE="\[\033[01;34m\]"
+YELLOW="\[\033[0;33m\]"
+ 
+PS_LINE=`printf -- '- %.0s' {1..200}`
 function parse_git_branch {
-	PS_BRANCH=""
-	if [ -d .svn ]; then
-		PS_BRANCH="(svn r$(svn info|awk '/Revision/{print $2}')) "
-		return
-	elif [ -f _FOSSIL_ -o -f .fslckout ]; then
-		PS_BRANCH="(fossil $(fossil status|awk '/tags/{print $2}')) "
-		return
-	fi
-	ref=$(git symbolic-ref HEAD 2> /dev/null) || return
-	PS_BRANCH="(git ${ref#refs/heads/}) "
+  PS_BRANCH=''
+  PS_FILL=${PS_LINE:0:$COLUMNS}
+  if [ -d .svn ]; then
+    PS_BRANCH="(svn r$(svn info|awk '/Revision/{print $2}'))"
+    return
+  elif [ -f _FOSSIL_ -o -f .fslckout ]; then
+    PS_BRANCH="(fossil $(fossil status|awk '/tags/{print $2}')) "
+    return
+  fi
+  ref=$(git symbolic-ref HEAD 2> /dev/null) || return
+  PS_BRANCH="(git ${ref#refs/heads/}) "
 }
-
-function parse_tlb_info {
-	parse_catkin_workspace
-	parse_git_branch
-}
-
-
-if [ ! -f ~/.disable_tlb ]; then
-	RESET="\[\033[0m\]"
-	RED="\[\033[0;31m\]"
-	GREEN="\[\033[01;32m\]"
-	BLUE="\[\033[01;34m\]"
-	YELLOW="\[\033[0;33m\]"
-
-	PROMPT_COMMAND=parse_tlb_info
-	PS_LINE=`printf -- "- %.0s" {1..200}`
-	PS_FILL=${PS_LINE:0:$COLUMNS}
-	PS_INFO="$GREEN\u@\h$RESET:$BLUE\w "
-	PS_CATKIN="$YELLOW\$PS_WORKSPACE"
-	PS_GIT="$YELLOW\$PS_BRANCH"
-	PS_TIME="\[\033[\$((COLUMNS-10))G\] $RED[\t]"
-	export PS1="\${PS_FILL}\[\033[0G\]${PS_INFO}${PS_CATKIN}${PS_GIT}${PS_TIME}\n${RESET}\$ "
-fi
+PROMPT_COMMAND=parse_git_branch
+PS_INFO="$GREEN\u@\h$RESET:$BLUE\w"
+PS_GIT="$YELLOW\$PS_BRANCH"
+PS_TIME="\[\033[\$((COLUMNS-10))G\] $RED[\t]"
+export PS1="\${PS_FILL}\[\033[0G\]${PS_INFO} ${PS_GIT}${PS_TIME}\n${RESET}\$ "
